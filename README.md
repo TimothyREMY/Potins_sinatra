999420f16f3f10e39291005a69b6094e47706c2a
TimothyREMY <timothy.remy@gmail.com>
2021-05-03 16:53:56 +0200

    My first and final commit I think

---
+++ README.md
@@ -0,0 +1,9 @@
+Avec cette magnifique application, vous aurez la possibilité de créer de nouveaux potins qui seront stockés dans une base de données CSV.
+
+Vous pourez en ajoutant vos potins décliner votre identité.
+
+Tout potin créé sera répertorier sur la page d'accueil de notre merveilleuse application. De là vous pourrez vous diriger vers les pages potins, chacune de ces pages compile l'ensemble des informations concernant tous ces malfaisants potins.
+
+De ces pages vous pourrez éditer vos potins, leurs modifications seront prises en compte dans notre base de données.
+
+Malheureusement vous ne pourrez pas commenter ces potins. On est contre les commentaires
---
+++ lib/gossip.rb
@@ -0,0 +1,36 @@
+class Gossip
+  attr_accessor :author, :content
+  
+  def initialize (author, content)
+    @author = author
+    @content = content
+  end
+
+  def save
+    CSV.open("./db/gossip.csv", "ab") do |csv|
+      csv << [author , content]
+    end
+  end
+
+  def self.all 
+    all_gossips = []
+    CSV.read("./db/gossip.csv").each do |csv_line|
+      all_gossips << Gossip.new(csv_line[0], csv_line[1])
+    end
+    return all_gossips
+  end
+
+  def self.find(id)
+    gossips = self.all
+    gossips[id.to_i]
+  end
+
+  def self.update(new_content, id)
+    gossips = self.all
+    gossips[id].content = new_content
+#Je vide le fichier et je le rempli avec les nouvelles valeurs
+		CSV.open("db/gossip.csv", "w") { |csv| }
+		CSV.open("db/gossip.csv", "w") { |csv| gossips.each { |gossip| gossip.save }}
+    
+  end
+end
